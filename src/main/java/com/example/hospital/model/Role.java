85cd95fcb46335475ce5a11ff471b5dffa9f67eb
ochwada <nakhulolinda@yahoo.com>
2025-06-30 16:56:17 +0200

    added  Role enum and Staff

---
+++ src/main/java/com/example/hospital/model/Role.java
@@ -0,0 +1,27 @@
+package com.example.hospital.model;
+
+
+/**
+ * *******************************************************
+ * Package: com.example.hospital.model
+ * File: Role.java
+ * Author: Ochwada
+ * Date: Monday, 30.Jun.2025, 4:43 PM
+ * Description: Represents the various user roles in the Hospital Management System.
+ * Objective:
+ * *******************************************************
+ */
+
+
+public enum Role {
+
+    // Can be Used to control access levels and assign responsibilities within the application.
+
+    DOCTOR,
+    SURGEON,
+    NURSE,
+    ADMIN,
+    LAB_TECHNICIAN,
+    PHARMACIST,
+    RECEPTIONIST
+}
